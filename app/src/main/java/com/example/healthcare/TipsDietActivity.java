67e843f7eafacf7bfbb3d88dac35e0601d219ebc
christoforadiana <christoforadianayuliawati@gmail.com>
2021-11-17 23:10:21 +0700

    Update File Folder

--- app/src/main/java/com/example/healthcare/TipsDietActivity.java
+++ app/src/main/java/com/example/healthcare/TipsDietActivity.java
@@ -1,7 +1,10 @@
 package com.example.healthcare;
 
+import static android.text.Layout.JUSTIFICATION_MODE_INTER_WORD;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
 
 public class TipsDietActivity extends AppCompatActivity {
