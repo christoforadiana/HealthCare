f615c5ba9ac5d8a212d615332c9f30073dfb4b41
Salsabillaayuni <salsabilaayuni31@gmail.com>
2021-12-14 11:40:18 +0700

    commit logout

---
+++ app/src/main/java/com/example/healthcare/SecondActivity.java
@@ -0,0 +1,56 @@
+package com.example.healthcare;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SecondActivity extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
+    private Button logout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        logout = (Button)findViewById(R.id.button_logout);
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    private void Logout(){
+        firebaseAuth.signOut();
+        finish();
+        startActivity(new Intent(SecondActivity.this, MainActivity.class));
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch(item.getItemId()){
+            case R.id.button_logout:{
+                Logout();
+            }
+            case R.id.btnLogin:
+                startActivity(new Intent(SecondActivity.this, ContactsContract.Profile.class));
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
